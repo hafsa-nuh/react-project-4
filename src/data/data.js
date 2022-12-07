df2d6db256aa3b13db30ec8735b4f366c731de4e
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-12-07 18:41:50 +0300

    added assets and data

---
+++ src/data/data.js
@@ -0,0 +1,222 @@
+import psale1 from "../assets/nike-air-red.png";
+import psale2 from "../assets/nike-adapt-bb.png";
+import psale3 from "../assets/nike-adapt-bb-smart.png";
+
+import sneakershoe from "../assets/sneaker.png";
+
+import hightlightimg from "../assets/hightlightimg.png";
+
+export const HeroSlider = [
+  {
+    src: " https://i.pinimg.com/originals/9b/d5/d2/9bd5d26051037621f2ab4f2079a0f65e.png",
+    content: {
+      h2: "Amazing Deals this Summer",
+      p: "Upto 40% off",
+    },
+    background: " bg-gradient-to-r from-cyan-500 to-blue-500",
+  },
+  {
+    src: "https://st3.depositphotos.com/7713882/17906/i/600/depositphotos_179068502-stock-photo-happy-woman-showing-good-big.jpg",
+    active: true,
+    content: {
+      h2: "Bhai mein dosry number wala hn",
+      p: "Upto 40% off",
+    },
+    background: " bg-[#ffd028]",
+  },
+];
+
+export const popularsales = {
+  title: "Popular Sales",
+  items: [
+    {
+      id: "0p0x1",
+      title: "Nike Addapt BB 2.0",
+      text: "MEN Running Shoes",
+      rating: "4.9",
+      btn: "Buy Now",
+      img: psale2,
+      price: "200",
+      color: "from-blue-600 to-blue-500",
+      shadow: "shadow-lg shadow-blue-500",
+    },
+    {
+      id: "0p0x2",
+      title: "Nike Martine Rose",
+      text: "MEN Running Shoes",
+      rating: "4.5",
+      btn: "Buy Now",
+      img: psale1,
+      price: "200",
+      color: "from-red-500 to-rose-500",
+      shadow: "shadow-lg shadow-rose-500",
+    },
+    {
+      id: "0p0x3",
+      title: "Nike Smart Shoe 2.0",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: psale3,
+      price: "200",
+      color: "from-violet-500 to-indigo-500",
+      shadow: "shadow-lg shadow-violet-500",
+    },
+  ],
+};
+
+export const sneaker = {
+  heading: "HOT DEAL",
+  titles: "NIKE SNEAKERS AIR LANCING SHOES",
+  text: "The radiance lives on Nike Sneakers Air Lancing Shoes, the basket ball OG that puts a fresh spin on what you know best: durably stitched overlays, clean finishes and the perfect amount of flash to make you shine.",
+  btn: "Explore More",
+  url: "https://sneakernews.com/2022/03/21/nike-lebron-2-retro-white-midnight-navy-varsity-crimson-dr0826-100/",
+  img: sneakershoe,
+};
+
+export const highlight = {
+  heading: "NEW LAUNCHES",
+  title: "NIKE AIR WITH LIMITLESS CHOICES",
+  text: "Our Purpose is to move the world forward. We take action by building community, protecting our planet and increasing access to sport.",
+  btn: "Explore More",
+  url: "https://www.nike.com/launch/t/nocta-hot-step-black-gold",
+  img: hightlightimg,
+};
+
+export const topratesales = {
+  title: "Top Rated Sales",
+  items: [
+    {
+      id: "0M0x1",
+      title: "Nike Air Low Premium",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/q75SxBZK/product7.png",
+      price: "150",
+      color: "from-sky-600 to-indigo-600",
+      shadow: "shadow-lg shadow-blue-500",
+    },
+    {
+      id: "0M0x2",
+      title: "Nike Air Force Green",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/mktxGzz8/product2.png",
+      price: "150",
+      color: "from-green-500 to-emerald-500",
+      shadow: "shadow-lg shadow-green-500",
+    },
+    {
+      id: "0M0x3",
+      title: "Nike Addapt BB Rose",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/9XSK7fqH/product3.png",
+      price: "150",
+      color: "from-red-500 to-rose-500",
+      shadow: "shadow-lg shadow-rose-500",
+    },
+    {
+      id: "0M0x4",
+      title: "Nike Air Premium",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/ZYBZg28Z/product4.png",
+      price: "150",
+      color: "from-orange-500 to-amber-500",
+      shadow: "shadow-lg shadow-orange-500",
+    },
+    {
+      id: "0M0x5",
+      title: "Nike Adapt BB Pro",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/LsQKQ3hn/product5.png",
+      price: "150",
+      color: "from-gray-900 to-yellow-500",
+      shadow: "shadow-lg shadow-yellow-500",
+    },
+    {
+      id: "0M0x6",
+      title: "Air Jordan PR3",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/m2WxnDZN/product6.png",
+      price: "150",
+      color: "from-blue-500 to-cyan-500",
+      shadow: "shadow-lg shadow-cyan-500",
+    },
+    {
+      id: "0M0x7",
+      title: "Nike Multi Smart Shoe",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/6pVrtHN1/product1.png",
+      price: "150",
+      color: "from-yellow-500 to-yellow-500",
+      shadow: "shadow-lg shadow-yellow-500",
+    },
+    {
+      id: "0M0x8",
+      title: "Nike Jordan Air Max",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/mgMPsh7L/product9.png",
+      price: "150",
+      color: "from-[#936550] to-orange-900",
+      shadow: "shadow-lg shadow-orange-800",
+    },
+    {
+      id: "0M0x9",
+      title: "Nike Old Max-x",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/rmpyKpRt/product10.png",
+      price: "150",
+      color: "from-indigo-700 to-indigo-700",
+      shadow: "shadow-lg shadow-indigo-500",
+    },
+    {
+      id: "0M0x10",
+      title: "Nike Lime Jordan 11",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/SQBXtWzT/product12.png",
+      price: "150",
+      color: "from-green-600 to-lime-500",
+      shadow: "shadow-lg shadow-lime-500",
+    },
+    {
+      id: "0M0x12",
+      title: "Nike Zoom Max",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/Xq02srKr/product8.png",
+      price: "150",
+      color: "from-blue-900 to-blue-500",
+      shadow: "shadow-lg shadow-blue-500",
+    },
+    {
+      id: "0M0x11",
+      title: "Nike Air Black Max",
+      text: "MEN Running Shoes",
+      rating: "5+",
+      btn: "Buy Now",
+      img: "https://i.postimg.cc/LXhRm0Gt/product11.png",
+      price: "150",
+      color: "from-slate-900 to-black",
+      shadow: "shadow-lg shadow-black",
+    },
+  ],
+};
