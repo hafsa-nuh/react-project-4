2131e764c82d76313238c1cdd38a9df96a99460e
yego <yegokipkemoi254@gmail.com>
2022-12-07 20:06:58 +0300

    footer layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import { Routes, Route } from "react-router-dom";
 import ProductsDetails from './components/products/ProductsDetails';
 import Navbar from './components/header/Navbar';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/products/:id" element={<ProductsDetails />} />
       </Routes>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+      <div className='container'>
+        <div className='row'>
+          <div className='footer-col'>
+           <h4>About Us</h4>
+           <ul>
+             <li>News</li>
+             <li>Oppoturnities</li>
+             <li>Purpose</li>
+             <li>Partners</li>
+           </ul>
+        </div>
+          <div className='footer-col'> 
+              <h4>Company</h4>
+              <ul>
+                <li>Find A Store</li>
+                <li>Promotions</li>
+                <li>Our Journal</li>
+                <li>Send Us Feedback</li>
+              </ul>
+          </div>
+          <div className='footer-col'> 
+              <h4>Get Help</h4>
+              <ul>
+                <li>Order Status</li>
+                <li>Shipping & Delivery</li>
+                <li>Payment Options</li>
+                <li>Gift Cards</li>
+                <li>Blog</li>
+                <li>FAQs</li>
+              </ul>
+          </div>
+        <div className='footer-col'>
+           <h4>Socials</h4>
+           <div className='social-links'>
+            
+            </div>
+         </div>
+        </div>
+        <div className='last'>
+          <p>Copyright &copy; {new Date().getFullYear()}, Moringa Devs | All rights reserved | Terms of Service | Privacy</p>
+          </div> 
+      </div>
+    </footer>
+    )
+}
+
+export default Footer;
